ebacd4827ee74ef36c37b45bdfe35c1cb61db132
faisaljutt07 <fasijatt12@gmail.com>
2023-11-28 15:56:48 +0500

    Update main.js

--- session-3/main.js
+++ session-3/main.js
@@ -1,1 +1,8 @@
-const batch = [{name:'', age: 0, email: '', phone: ''}]
+const batch = [{ name: 'Faisal', age: 23, email: 'fasijatt12@gmail.com', phone: '03123456677' },
+{ name: 'Faisal', age: 23, email: 'fasijatt12@gmail.com', phone: '03123456677' },
+ { name: 'Abbas', age: 23, email: 'fasijatt12@gmail.com', phone: '03123456677' },
+  { name: 'usman', age: 23, email: 'fasijatt12@gmail.com', phone: '03123456677' },
+   { name: 'moeez', age: 23, email: 'fasijatt12@gmail.com', phone: '03123456677' },
+    { name: 'bisma', age: 23, email: 'fasijatt12@gmail.com', phone: '03123456677' },
+]
+console.log ("const");
