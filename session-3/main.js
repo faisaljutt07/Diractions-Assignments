34a68bae60c92ca53c8b8d50361e64ea7e549389
faisaljutt07 <fasijatt12@gmail.com>
2023-11-28 21:34:05 +0500

    change age

--- session-3/main.js
+++ session-3/main.js
@@ -1,9 +1,9 @@
 const batch =
     [{ name: 'Faisal', age: 23, email: 'fasijatt12@gmail.com', phone: '03123456677' },
-    { name: 'Faisal', age: 23, email: 'abc.12@gmail.com', phone: '03123456677' },
-    { name: 'Abbas', age: 23, email: 'abc.12@gmail.com', phone: '03123456677' },
-    { name: 'usman', age: 23, email: 'abc.1212@gmail.com', phone: '03123456677' },
-    { name: 'moeez', age: 23, email: 'xyz.jatt12@gmail.com', phone: '03123456677' },
-    { name: 'bisma', age: 23, email: 'ghj.2@gmail.com', phone: '03123456677' },
+    { name: 'Faisal', age: 24, email: 'abc.12@gmail.com', phone: '03123456677' },
+    { name: 'Abbas', age: 25, email: 'abc.12@gmail.com', phone: '03123456677' },
+    { name: 'usman', age: 26, email: 'abc.1212@gmail.com', phone: '03123456677' },
+    { name: 'moeez', age: 27, email: 'xyz.jatt12@gmail.com', phone: '03123456677' },
+    { name: 'bisma', age: 28, email: 'ghjnnjnnnj.2@gmail.com', phone: '03123456677' },
     ]
 console.log("const");
